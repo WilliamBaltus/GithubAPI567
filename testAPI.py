0e136c62c89a7380dde468260eadfe662e5f5006
WilliamBaltus <wbaltus21@gmail.com>
2021-10-02 17:31:47 -0400

    Update testAPI.py

--- testAPI.py
+++ testAPI.py
@@ -9,6 +9,9 @@ class TestAPIs(unittest.TestCase):
 
     def testUserNameB(self): 
         self.assertEqual(repo_info('richkempinski1'), 'User Not Found')
+    
+    def testRepoCommits(self):
+        self.assertEqual(repo_info('WilliamBaltus'), "Repos: 10 Commits: 80")
 
 if __name__ == '__main__':
     print('Running unit tests')
