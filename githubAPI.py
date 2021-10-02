00f8667a87eb4f4dfab6668f5ebc7e06cb58291b
WilliamBaltus <wbaltus21@gmail.com>
2021-10-02 17:17:40 -0400

    Update githubAPI.py

--- githubAPI.py
+++ githubAPI.py
@@ -5,6 +5,12 @@ def repo_info(username):
     if (type(username) == str):
         repos = "https://api.github.com/users/" + username + "/repos"
         repoJson = requests.get(repos).json()
+        #check for non-existant user!
+        counter = 0
+        for json in repoJson:
+            counter += 1
+        if (counter == 2):
+            return "User Not Found"
 
         for json in repoJson:
             counter = 0
@@ -14,6 +20,8 @@ def repo_info(username):
             for json in commitJson:
                 counter = counter + 1
             print("Commits: " + str(counter))
+    else: 
+        return "Invalid User Format"
 
 
 repo_info('WilliamBaltus')
