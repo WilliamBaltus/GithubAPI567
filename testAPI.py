717c75f80a9118455c5dbe985c13dcbb38b713dd
WilliamBaltus <wbaltus21@gmail.com>
2021-10-15 13:00:15 -0400

    added mock-- unknown bug

--- testAPI.py
+++ testAPI.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest import mock
+import unittest.mock
 
 mockedReq = mock.Mock()
 
@@ -7,23 +8,24 @@ from githubAPI import repo_info
 
 class TestAPIs(unittest.TestCase):
 
-    def testUserNameA(self): 
-        self.assertEqual(repo_info(123), 'Invalid User Format')
+    # def testUserNameA(self): 
+    #     self.assertEqual(repo_info(123), 'Invalid User Format')
 
-    def testUserNameB(self): 
+    # def testUserNameB(self): 
+    #     self.assertEqual(repo_info('richkempinski1'), 'User Not Found')
+    
+    # def testRepoCommits(self):
+    #     self.assertEqual(repo_info('WilliamBaltus'), "Repos: 10 Commits: 85")
+    
+
+    @mock.patch('requests.get')
+    def testUserName(self, mockedReq):
+        expected = {
+            "message": "Not Found",
+            "documentation_url": "https://docs.github.com/rest/reference/repos#list-repositories-for-a-user"
+        }
+        mockedReq.return_value = expected
         self.assertEqual(repo_info('richkempinski1'), 'User Not Found')
-    
-    def testRepoCommits(self):
-        self.assertEqual(repo_info('WilliamBaltus'), "Repos: 10 Commits: 83")
-    
-    expected = {
-        "message": "Not Found",
-        "documentation_url": "https://docs.github.com/rest/reference/repos#list-repositories-for-a-user"
-    }
-
-    @mock.patch('requests.get', return_value = expected)
-    def testInvalidUser(self, mockedReq):
-        with self.assertRaises(TypeError): repo_info('richkempinski1')
 
 if __name__ == '__main__':
     print('Running unit tests')
