63e7d43845a2931aba9b0783becb8a25ca194ceb
WilliamBaltus <wbaltus21@gmail.com>
2021-10-15 12:29:24 -0400

    added mock

--- testAPI.py
+++ testAPI.py
@@ -1,4 +1,7 @@
 import unittest
+from unittest import mock
+
+mockedReq = mock.Mock()
 
 from githubAPI import repo_info
 
@@ -12,6 +15,15 @@ class TestAPIs(unittest.TestCase):
     
     def testRepoCommits(self):
         self.assertEqual(repo_info('WilliamBaltus'), "Repos: 10 Commits: 83")
+    
+    expected = {
+        "message": "Not Found",
+        "documentation_url": "https://docs.github.com/rest/reference/repos#list-repositories-for-a-user"
+    }
+
+    @mock.patch('requests.get', return_value = expected)
+    def testInvalidUser(self, mockedReq):
+        with self.assertRaises(TypeError): repo_info('richkempinski1')
 
 if __name__ == '__main__':
     print('Running unit tests')
